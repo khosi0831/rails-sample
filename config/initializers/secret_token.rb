6bd70efb40acbe278cc406c148e753e749fa54b3
root <root@hoshi-ThinkPad-R50p.(none)>
2012-08-19 15:16:57 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthSample::Application.config.secret_token = 'ce81110685373f47d855b0582def5b22f7e98e2b5a9f7f1166710e658cd0e6c6ea8be0cb999289e3f61896e1f6afb7a2f438986d95f29f3f15be668638150e2d'
